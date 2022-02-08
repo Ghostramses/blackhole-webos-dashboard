a2f9febc3e63d1fa07bce5ec50e192cb59c561b6
Alfredo Rafael González Rodríguez <alfredo.gonzalez@octopy.com>
2022-02-08 09:47:10 -0600

    Changed some axios calls to XMLHttpRequest

--- src/components/CaseCutGraph/CaseCutGraph.js
+++ src/components/CaseCutGraph/CaseCutGraph.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { getCaseCutGraph } from '../../providers/api/requests';
 import Scroller from '@enact/moonstone/Scroller';
+import { Job } from '@enact/core/util';
+import { config } from '../../providers/config';
 
 import css from './CaseCutGraph.less';
 
@@ -20,19 +21,39 @@ export default class CaseCutGraph extends Component {
   }
 
   getCaseCut() {
-    getCaseCutGraph()
-      .then(response => {
-        const { morning } = response.items[0];
-        const { evening } = response.items[1];
-        const { night } = response.items[2];
-        this.setState({ morning, evening, night });
-      })
-      .catch(e => console.error(e))
-      .finally(() => {
-        this.timeoutId = setTimeout(() => {
-          this.getCaseCut();
-        }, 1500);
-      });
+    const onOk = function (response) {
+      const { morning } = response.items[0];
+      const { evening } = response.items[1];
+      const { night } = response.items[2];
+      this.setState({ morning, evening, night });
+    }.bind(this);
+    const onCallback = function () {
+      this.getCaseCut();
+    }.bind(this);
+    const job = new Job(onCallback, 1500);
+    const onFinally = function () {
+      job.start();
+    };
+    const xhr = new XMLHttpRequest(); //eslint-disable-line
+    xhr.open('GET', config.mainBackendUrl + '/case_cut_graph');
+    xhr.setRequestHeader('token', localStorage.token); //eslint-disable-line
+    xhr.onreadystatechange = function () {
+      //eslint-disable-next-line
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const response = JSON.parse(this.response);
+        onOk(response);
+        onFinally();
+      } else {
+        onFinally();
+      }
+    };
+    xhr.onerror = function () {
+      onFinally();
+    };
+    xhr.ontimeout = function () {
+      onFinally();
+    };
+    xhr.send();
   }
 
   render() {
--- src/components/EventsGraph/EventsGraph.js
+++ src/components/EventsGraph/EventsGraph.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import Counter from '../Counter';
-import {
-  getLiveEventsGraph,
-  getTotalEventsGraph
-} from '../../providers/api/requests';
+import { Job } from '@enact/core/util';
+import { config } from '../../providers/config';
 
 import css from './EventsGraph.less';
 
@@ -29,40 +27,80 @@ export default class EventsGraph extends Component {
   }
 
   getEvents() {
-    getTotalEventsGraph()
-      .then(response => {
-        const { discarded, in_follow_up_evidences, not_relevant, relevant } =
-          response.items[0];
-        this.setState({
-          relevant,
-          unrelevant: not_relevant,
-          following: in_follow_up_evidences,
-          trash: discarded
-        });
-      })
-      .catch(e => console.error(e))
-      .finally(() => {
-        this.timeoutIdClassificatedEvents = setTimeout(() => {
-          this.getEvents();
-        }, 1500);
+    const onOk = function (response) {
+      const { discarded, in_follow_up_evidences, not_relevant, relevant } =
+        response.items[0];
+      this.setState({
+        relevant,
+        unrelevant: not_relevant,
+        following: in_follow_up_evidences,
+        trash: discarded
       });
+    }.bind(this);
+    const onCallback = function () {
+      this.getEvents();
+    }.bind(this);
+    const job = new Job(onCallback, 1500);
+    const onFinally = function () {
+      job.start();
+    };
+    const xhr = new XMLHttpRequest(); //eslint-disable-line
+    xhr.open('GET', config.mainBackendUrl + '/events_graph');
+    xhr.setRequestHeader('token', localStorage.token); //eslint-disable-line
+    xhr.onreadystatechange = function () {
+      //eslint-disable-next-line
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const response = JSON.parse(this.response);
+        onOk(response);
+        onFinally();
+      } else {
+        onFinally();
+      }
+    };
+    xhr.onerror = function () {
+      onFinally();
+    };
+    xhr.ontimeout = function () {
+      onFinally();
+    };
+    xhr.send();
   }
 
   getAliveEvents() {
-    getLiveEventsGraph()
-      .then(response => {
-        const [unclassified, alive] = response.items;
-        this.setState({
-          noClassified: unclassified.total,
-          alive: alive.total
-        });
-      })
-      .catch(e => console.error(e))
-      .finally(() => {
-        this.timeoutIdAliveEvents = setTimeout(() => {
-          this.getAliveEvents();
-        }, 1500);
+    const onOk = function (response) {
+      const [unclassified, alive] = response.items;
+      this.setState({
+        noClassified: unclassified.total,
+        alive: alive.total
       });
+    }.bind(this);
+    const onCallback = function () {
+      this.getAliveEvents();
+    }.bind(this);
+    const job = new Job(onCallback, 1500);
+    const onFinally = function () {
+      job.start();
+    };
+    const xhr = new XMLHttpRequest(); //eslint-disable-line
+    xhr.open('GET', config.mainBackendUrl + '/events_graph/hard');
+    xhr.setRequestHeader('token', localStorage.token); //eslint-disable-line
+    xhr.onreadystatechange = function () {
+      //eslint-disable-next-line
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const response = JSON.parse(this.response);
+        onOk(response);
+        onFinally();
+      } else {
+        onFinally();
+      }
+    };
+    xhr.onerror = function () {
+      onFinally();
+    };
+    xhr.ontimeout = function () {
+      onFinally();
+    };
+    xhr.send();
   }
 
   render() {
